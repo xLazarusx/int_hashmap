0f83b7531d22a2a460f603c23086df020ce5c2cf
Laurenzius <74829384+xLazarusx@users.noreply.github.com>
2026-06-23 20:45:00 +0200

    deleted other hashing functions

--- src/hash_functions.h
+++ src/hash_functions.h
@@ -4,8 +4,5 @@
 #include <stddef.h>
 
 int int_hashing(int *num, int hashmap_size);
-int float_hashing(float num, int hashmap_size);
-int string_hashing(char* string, size_t size, int hashmap_size);
-int compound_hashing(int* compound_key, size_t size, int hashmap_size);
 
 #endif
